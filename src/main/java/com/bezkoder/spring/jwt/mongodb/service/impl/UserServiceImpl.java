a91f0df9c5c8588370cecee0c439df8d30f6dc4e
twappleon <twappleon@gmail.com>
2021-11-10 23:52:45 +0800

    change error

--- src/main/java/com/bezkoder/spring/jwt/mongodb/service/impl/UserServiceImpl.java
+++ src/main/java/com/bezkoder/spring/jwt/mongodb/service/impl/UserServiceImpl.java
@@ -34,7 +34,7 @@ public class UserServiceImpl implements UserService {
     public UserResponse add(UserAddRequest userAddRequest) {
         Optional<User> optional = userRepository.findById(userAddRequest.getId());
         if(optional.isPresent()){
-            throw  new BusinessLogicException(StatusEnum.USER_NOT_EXIST.getValue(),StatusEnum.USER_NOT_EXIST.getInfo(), HttpStatus.INTERNAL_SERVER_ERROR);
+            throw  new BusinessLogicException(StatusEnum.USER_EXIST.getValue(),StatusEnum.USER_EXIST.getInfo(), HttpStatus.INTERNAL_SERVER_ERROR);
         }
         User user = new User();
         BeanUtils.copyProperties(userAddRequest,user);
